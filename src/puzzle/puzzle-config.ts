3a39208749909a6f689d413b398498448b8174fa
namewxz <wxz2307152768@outlook.com>
2026-03-28 10:17:15 +0800

    trae fix3

--- src/puzzle/puzzle-config.ts
+++ src/puzzle/puzzle-config.ts
@@ -209,7 +209,7 @@ abstract class PuzzleConfigForm extends SignalWatcher(LitElement) {
     // If the form has already been rendered, re-rendering with new value attributes
     // won't update input element state. Flush current values into item properties.
     for (const [id, item] of Object.entries(this.config?.items as any ?? [])) {
-      const type = item.type;
+      const type = (item as any).type;
       const value = this.changes[id] ?? this.values[id];
       if (value !== undefined) {
         for (const element of this.shadowRoot?.querySelectorAll<HTMLInputElement>(
